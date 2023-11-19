7d53c57a3f15ab5a38caea769aae412e7511e6e7
CodeMagnit <mz8184107@gmail.com>
2023-11-19 19:17:46 +0500

    program13.7

---
+++ program13.7.cpp
@@ -0,0 +1,16 @@
+/*write a program that display a simple message on the screen whenever an objecto of that class is created*/
+#include<iostream>
+using namespace std;
+class love
+{
+    public:
+    love()
+    {
+        cout<<"Object is Created Successfully.............."<<endl;
+    }
+};
+int main()
+{
+    love a,e,i,o,u;
+    return 0;
+}
---
+++ program13.8.cpp
@@ -0,0 +1,1 @@
+/*Write a class that contains two integer data members which are initialize to 100 when an object is created. It has a member function avg that displays the average of data memebers*/
